d1ef6878208335a5a0ed4d610c455759362fd9c9
Kevin Tabak <kevin_tabak@outlook.com>
2020-04-14 15:18:32 +0200

    Some styling changes Added text to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,10 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="flex-center position-ref">
+<div class="">
     <div class="content">
         <div class="m-b-md">
-            <h1 class="heading">About me</h1>
+            <div class="container">
+                <h1 class="heading">About me</h1>
+                <p>A young professional in the making!</p>
+                <p>
+                    Hi there! My name is Kevin Tabak and I aspire to be a professional software engineer. <br>
+                    Currently I am still studying software engineering at the NHL Stenden in Emmen. <br>
+                    Even though I am not very experienced in working I am ready to learn. I got some basic knowladge in programming
+                    and other IT and hope to learn even more!
+                </p>
+
+                <h2>Skills</h2>
+                <p>Even with the lack of experience I have some skills I am proud to share with you!</p>
+                <span class="note">*The measures are based on my school results</span>
+            </div>
+
+
         </div>
     </div>
 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,18 +6,17 @@
             <a class="navbar-brand" href="{{ url('/') }}">
                 <span>Kevin Tabak</span>
             </a>
+            <!-- Hamburger button when nav is collapsed -->
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-
-                </ul>
-
                 <!-- Right Side Of Navbar -->
                 <ul class="navbar-nav ml-auto">
+                    <a href="{{ route('about') }}" class="nav-link {{ (request()->is('about*')) ? 'active' : '' }}">About</a>
+                    <a href="{{ route('portfolio') }}" class="nav-link {{ (request()->is('portfolio*')) ? 'active' : '' }}">portfolio</a>
+                    <a href="{{ route('contact') }}" class="nav-link {{ (request()->is('contact*')) ? 'active' : '' }}">Contact</a>
                     <!-- Logout Links -->
                     @guest
                     @else
@@ -32,7 +31,6 @@
                                                  document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
-
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                     @csrf
                                 </form>
@@ -43,22 +41,8 @@
             </div>
         </div>
     </nav>
-    <!-- Lower navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark shadow-sm second-navbar">
-        <div class="container">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    <a href="/" class="nav-link">Home</a>
-                    <a href="{{ route('about') }}" class="nav-link {{ (request()->is('about*')) ? 'active' : '' }}">About</a>
-                    <a href="{{ route('portfolio') }}" class="nav-link {{ (request()->is('portfolio*')) ? 'active' : '' }}">portfolio</a>
-                    <a href="{{ route('contact') }}" class="nav-link {{ (request()->is('contact*')) ? 'active' : '' }}">Contact</a>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
-    <main class="py-4">
+    <main class="py-4 main">
         @yield('content')
     </main>
 </div>
